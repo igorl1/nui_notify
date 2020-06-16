673295ec327bbee5bcd4a171860a1336e16dd18d
Igor Oliveira <56984151+igorl1@users.noreply.github.com>
2020-06-16 12:03:14 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	"html/main.js",
+	"html/index.html",
+	"html/main.css",
+	"html/assets/demo.ogg",
+	"html/assets/*.png"
+}
+
+ui_page 'html/index.html'
